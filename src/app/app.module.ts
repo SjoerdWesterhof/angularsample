5f957977b99e80080f76089e3947d4f061a2a758
Westerhof <sjoerd.westerhof@capgemini.com>
2020-03-23 15:53:14 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router'
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { EventsListComponent } from './events-list.component';
+import { CreateEventComponent } from './create-event.component';
+import { EventService } from './event.service';
+import { appRoutes } from './routes'
 
 @NgModule({
+  imports: [ 
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsListComponent,
+    CreateEventComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ EventService ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+
+}
+
